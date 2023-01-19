f827e3578d9b0b198270c6cd149e5f1bdb3590e6
isabella709806 <122543587+isabella709806@users.noreply.github.com>
2023-01-19 09:51:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showArrow(ArrowNames.East)
+    }
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("OK!")
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Angry)
+})
+basic.clearScreen()
